acfc1f31ac7dce3ff39f8d81d85f740a9013ba76
Lucrezia Valeriani <lucrezia.valeriani@areasciencepark.it>
2022-11-29 11:56:47 +0100

    id

--- intrinsic_dimension.py
+++ intrinsic_dimension.py
@@ -15,6 +15,13 @@ import pandas as pd
 import argparse
 from argparse import Namespace
 import random    
+
+import plotly.graph_objects as go
+from plotly.graph_objects import Layout
+import plotly.io as pio
+from plotly.subplots import make_subplots
+import seaborn as sns
+import plotly.express as px
               
 def estimate(X,fraction=0.8,verbose=False):    
     # sort distance matrix
@@ -72,9 +79,7 @@ def estimate(X,fraction=0.8,verbose=False):
     return x,y,regr.coef_[0][0],r,pval,npoints                    
   
                       
-def block_analysis(X, resdir, name, blocks=list(range(1, 21)), fraction=0.8):
-    #fitfile = open(resdir+'fit-res-'+name+".txt", "w")
-    
+def block_analysis(X, blocks=list(range(1, 21)), fraction=0.8):    
     n = X.shape[0]
     dim = np.zeros(len(blocks))
     std = np.zeros(len(blocks))
@@ -89,115 +94,41 @@ def block_analysis(X, resdir, name, blocks=list(range(1, 21)), fraction=0.8):
         for i in range(b):
             I = np.meshgrid(split[i], split[i], indexing='ij')
             tX = X[tuple(I)]
-            x,y,reg,r,pval,npoints = estimate(tX,fraction=fraction,verbose=False)
-            
-            if resdir != '':
-                filename = resdir+name+'-fit'+str(b)+'-'+str(i)+'.png'
-            
-                fig = plt.figure(figsize=(6,6)) 
-                plt.plot(x[0:npoints], y[0:npoints], '.', markersize = 7, alpha=0.3, label = 'datapoints')
-                plt.plot(x[0:npoints], reg*x[0:npoints], label = 'linear fit')
-            
-                plt.legend()
-                plt.tight_layout()
-                plt.savefig(filename,dpi=200)
-                plt.close()
-   
-            #fit for non-correlation -> plot x,y
-            #fitfile.write('block:'+str(b)+', trial:'+str(i)+', pearson_coeff:'+str(r)+', pvalue:'+str(pval)+'\n')
+            _,_,reg,_,_,npoints = estimate(tX,fraction=fraction,verbose=False)
             tdim[i] = reg          
-        
         dim[blocks.index(b)] = np.mean(tdim)
         std[blocks.index(b)] = np.std(tdim)
         n_points.append(npoints)
     
-    #fitfile.close()
     return dim,std,n_points
 
 
-
-def save_ID_results(filename, dim, std, n_points):
-    block_df = pd.DataFrame(columns =  ['dim','std', 'n_points'])
+def save_ID_results(dim, std, n_points, filename = '', save = False):
+    block_df = pd.DataFrame(columns =  ['dim','std','n_points'])
     block_df['dim'] = dim
     block_df['std'] = std
     block_df['n_points'] = n_points
-    #block_df.to_csv(filename, sep = '\t', index = False)
+    if save:
+        if filename != '':
+            block_df.to_csv(filename, sep = '\t', index = False)
+        else:
+            'Missing filename!'
     return block_df
 
 
-
-def plot_ID_graph(filename, block_df, scaled, title = None):
-    if title is None:
-        title = 'ID_graph'
-    
-    fig = plt.figure(figsize=(10,6))
-    plt.title(title)
-    
-    if scaled: 
-        index = [i for i in range(len(block_df))]
-        index.reverse()
-        block_df['index'] = index
-
-        plt.xticks(block_df['index'][::-1], labels = block_df['n_points'][::-1], rotation = 'vertical')
-        plt.ylim(bottom= 0, top = max(block_df['dim'])+2)
-        plt.errorbar(block_df['index'][::-1], block_df['dim'][::-1], block_df['std'][::-1])
-    
-    else:
-        plt.xticks(block_df['n_points'][::-1], labels = block_df['n_points'][::-1], rotation = 'vertical')
-        plt.ylim(bottom= 0, top = max(block_df['dim'])+2)
-        plt.errorbar(block_df['n_points'][::-1], block_df['dim'][::-1], block_df['std'][::-1])
-    
-    plt.grid()
-    plt.xlabel('N points')
-    plt.ylabel('ID')
-    plt.tight_layout()
-    
-    plt.savefig(filename,dpi=200)
-    plt.close()
+def plot_curve_ID(fig,reps,mean,cline,name,r=1,c=1,legend=True):
+    fig.add_trace(go.Scatter(x=reps,
+                            y=mean,
+                            mode='lines+markers',
+                            name = name,
+                            marker = dict(color=cline, size=9),
+                            line = dict(color=cline,width=3),
+                            showlegend=legend,
+                            
+    ),row=r, col=c)
+    return fig
 
 
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-input", "--inputdir", default='/u/area/lvaleriani/scratch/pnet-MSA/res/mean/pnet-rep-0.npy', type=str, help="input")
-    parser.add_argument("-output", "--outputdir", default='', type=str, help="output")
-      
-    args = parser.parse_args()
-    input = args.inputdir 
-    output = args.outputdir
-    
-    random.seed(609) 
-    
-    name = input.split('/')[-1].split('-')[0]
-    rep = ''.join(input.split('/')[-1].split('-')[1:3]).split('.')[0]
-    print(rep,name)
 
-    res = output + str(rep) + '/' 
-    if not os.path.exists(res):
-        os.makedirs(res,exist_ok = True)
-    
-    res_id = output + '/ids/'
-    os.makedirs(res_id,exist_ok=True)
-   
-    d = input
-    mean = np.load(d)
-    print(mean.shape)
-    print(mean,'\n\n\n')
-
-    X = pdist(mean, 'euclidean')
-    dist_mat = squareform(X)
-        
-    name_rep = name
-    dim,std,n_point = block_analysis(dist_mat, res, name_rep, blocks=list(range(1, 21)), fraction=0.9)
-
-    idfile = open(res_id +'id-'+name+'-'+str(rep)+".txt", "w")
-    idfile.write('id-dimension: '+ str(dim) +'\n'+ 'mean-id-dimension: '+ str(sum(dim)/len(dim)) +
-            '\n' +'std-dev: '+ str(std) + '\n'+'n_point: '+ str(n_point))
-    idfile.close()
-
-    block_df = save_ID_results(res+name+'-blockdf', dim, std, n_point)
-
-    title = 'ID-'+name+'rep-'+str(rep)
-    scaled = True
-    plot_ID_graph(res+name+'-plot-ID-'+str(rep), block_df, scaled, title)
