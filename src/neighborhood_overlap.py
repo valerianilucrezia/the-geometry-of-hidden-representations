4ecdc3af556c5e85e30ae986448220d79d7e2e62
Lucrezia Valeriani <lucrezia.valeriani@areasciencepark.it>
2022-12-01 12:57:46 +0100

    add plot no functions

--- src/neighborhood_overlap.py
+++ src/neighborhood_overlap.py
@@ -1,5 +1,19 @@
 import numpy as np
 import plotly.graph_objects as go
+import plotly.io as pio
+
+
+def get_data(path, label_path, layer, ng=100):
+    ds = create_ds(layer)
+    for i in np.arange(0, layer+1):
+        neig = np.load(path + str(i) + '-neigh.npy')
+        idx = np.load(path + str(i) + '-idx.npy')
+
+        label = open(label_path + 'sp_lab.txt')
+        label = np.array([l.strip('\n') for l in label])
+
+        ds = add_data(ds, i, neig, idx, label, ng)
+    return ds
 
 
 def create_ds(nlayer):
@@ -14,15 +28,14 @@ def add_data(ds, layer, distances, idxs, labels, knn):
     return ds
 
 
-def overlap_mean(ds, k):
+def overlap_label(ds, k):
     overlap_alls = []
 
     for l in ds.keys():
         overlap = 0.
 
-        distances = ds[l]['distances']
-        nelem = distances.shape[0]
-        idxs = ds[l]['idxs']
+        idxs = ds[l]['idxs'] 
+        nelem = idxs.shape[0]
         gt_labels = ds[l]['labels']
 
         for i in range(nelem):
@@ -34,9 +47,15 @@ def overlap_mean(ds, k):
     return overlap_alls
 
 
-def overlap_layer():
-    overlap_alls = []
-    return overlap_alls
+def overlap_layer(ds, l1, l2, k):
+    idxs_1 = ds[l1]['idxs'] 
+    idxs_2 = ds[l2]['idxs'] 
+
+    nelem = idxs_1.shape[0]
+    ov = []
+    for i in range(nelem):
+        ov.append(len(set(idxs_1[i, 1:k + 1]) & set(idxs_2[i, 1:k + 1])))
+    return np.mean(ov) /k
 
 
 def plot_no(fig, layer, df, color, lab, legend=True):
@@ -50,4 +69,44 @@ def plot_no(fig, layer, df, color, lab, legend=True):
                             showlegend = legend
                         )
                 )
+    return fig
+
+def update_figure(fig, title, save = False, name = ''):
+    fig.update_xaxes(showline = True, 
+                    linewidth = 1, 
+                    linecolor = 'black',
+                    tickvals = np.arange(0, 1.1, 0.1),
+                    range = [-0.01,1.02],
+                    title = 'relative depth',
+                    ticks = 'outside'
+                    )
+
+    fig.update_yaxes(showline = True, 
+                    linewidth = 1, 
+                    linecolor = 'black', 
+                    tickvals = np.arange(0, 1.1, 0.1), 
+                    range = [0,1.02], 
+                    title = title, 
+                    ticks = 'outside'
+                    )
+    w = 600
+    h = 500
+    fig.update_layout(width = w, 
+                    height = h, 
+                    font = dict(size = 12),
+                    legend = dict(orientation = "h",
+                              yanchor = "top",
+                              y = 1.12,
+                              xanchor = "center",
+                              x = 0.5, 
+                              font = dict(size = 12)
+                              )
+                    ) 
+
+    if save and name != '': 
+        pio.write_image(fig, 
+                        name, 
+                        scale=5, 
+                        width=w, 
+                        height=h)
     return fig
